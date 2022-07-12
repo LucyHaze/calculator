9198b7a59364d9011fed92b8b84a119bd383c475
Lucy Haze <animationenju@gmail.com>
2022-07-12 10:25:16 +0200

    changed html structure, added reading numbers from clicked buttons

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const formContainer =  document.querySelector('form');
+const buttonsContainer =  document.querySelector('.buttons');
+const displayElement = document.querySelector('[name="display"]');
+const sequenceArray = [0];
 
-formContainer.addEventListener('click', function(eventObject) {
-	console.log(eventObject.target)
+buttonsContainer.addEventListener('click', function(eventObject) {
+	console.log(eventObject.target.value);
+	const currentDisplayValue = displayElement.value
+	displayElement.value = currentDisplayValue + eventObject.target.value;
+	
+	if (!isNaN (parseInt(eventObject.target.value,10))){
+		const lastElement = sequenceArray[sequenceArray.length-1]; 
+		console.log(typeof lastElement, sequenceArray);
+		if (typeof lastElement === 'number'){
+			const newValue = (lastElement ? lastElement.toString() : '');
+			sequenceArray[sequenceArray.length-1] = parseInt(newValue + eventObject.target.value);
+			console.log(sequenceArray);				
+		}		
+	}
+	
 
 })
